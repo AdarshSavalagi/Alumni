840977a78483f2678f53ac208956413ec348f5e6
AdarshSavalagi <adarshsavaligi@gmail.com>
2024-06-11 23:03:34 +0530

    chore: Add react-slick and slick-carousel dependencies for image carousel in Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,59 @@ import { motion } from "framer-motion";
 import Hero from "@/Components/home/Hero";
 import Container2 from "@/Components/home/Container2";
 import Container3 from "@/Components/home/Container3";
-import Container5 from "@/Components/home/Container5";
 import HrTag from "@/Components/components/HrTag";
-import NewsCard from "@/Components/home/NewsCard";
-export default function Home() {
+import OfficeBearers from "@/Components/home/OfficeBearers";
 
+const testis=[
+  {
+    name:'Name',
+    role:"student",
+    avatar:"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp",
+    "text":"1",
+    rating:3
+  }, {
+    name:'Name',
+    role:"student",
+    avatar:"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp",
+    "text":"lorem23",
+    rating:3
+  }, {
+    name:'Name',
+    role:"student",
+    avatar:"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp",
+    "text":"lorem23",
+    rating:3
+  }, {
+    name:'Name',
+    role:"student",
+    avatar:"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp",
+    "text":"lorem23",
+    rating:3
+  }, {
+    name:'Name',
+    role:"student",
+    avatar:"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp",
+    "text":"lorem23",
+    rating:3
+  }, {
+    name:'Name',
+    role:"student",
+    avatar:"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp",
+    "text":"lorem23",
+    rating:3
+  },
+];
+
+export default function Home() {
   return (
     <motion.div className="">
       <Hero />
       <main>
         <Container2 />
         <HrTag />
+        <OfficeBearers testimonials={testis}/>
+     
         <Container3 />
-        <HrTag />
-        <Container5 />
-        <HrTag />
-        <NewsCard />
       </main>
     </motion.div>
   );
