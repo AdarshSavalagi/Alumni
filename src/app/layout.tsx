57e3564e1f9761ef4b902b4fc2afc7b38ee7d1c6
AdarshSavalagi <adarshsavaligi@gmail.com>
2024-06-18 00:17:37 +0530

    refactor: Improve code formatting and fix minor issues in layout.tsx, OfficeBearers.tsx, and Container3.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} >
         <div className="h-screen overflow-y-auto" id="body">
-      <Navbar/>
-      {children}
-      <Footer/>
+          <Navbar />
+          {children}
+          <Footer />
         </div>
       </body>
     </html>
