27769aa1491d91bc83c1ede5ca5b24256900fc48
AdarshSavalagi <adarshsavaligi@gmail.com>
2024-06-22 23:30:39 +0530

    feat: Add lazy loading for images and custom scrollbar styles

--- src/Components/navbar/Navbar.tsx
+++ src/Components/navbar/Navbar.tsx
@@ -41,16 +41,30 @@ const navItemVariants = {
   }
 };
 
-const links = ["Community", "Giving", "Program & Event", "College", "About Us"];
+// const links = ["Community", "Giving", "Program & Event", "College", "About Us"];
+const links = [
+  {
+    name: 'Alumni',
+    link: '/alumni'
+  },
+  {
+    name: 'College',
+    link: "https://sitmng.ac.in"
+  },
+  {
+    name:'News',
+    link:'#news'
+  }
+];
 
 export default function Navbar() {
-const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-const NavItem = ({ children }: { children: React.ReactNode }) => (
+  const NavItem = ({ link, href }: { link: string, href: string }) => (
     <motion.li variants={navItemVariants} whileHover='hover' className="my-auto">
-        <Link href="/">{children}</Link>
+      <Link href={href}>{link}</Link>
     </motion.li>
-);
+  );
 
   return (
     <>
@@ -59,15 +73,15 @@ const NavItem = ({ children }: { children: React.ReactNode }) => (
           variants={navItemVariants}
           initial="hidden"
           animate="visible"
-          transition={{ duration: 0.01, type: "spring", stiffness: 100, ease: "easeInOut", staggerChildren: 0.01, delayChildren: 0.01, when: "beforeChildren" }} 
+          transition={{ duration: 0.01, type: "spring", stiffness: 100, ease: "easeInOut", staggerChildren: 0.01, delayChildren: 0.01, when: "beforeChildren" }}
           className="nav-container bg-white h-16 md:h-20 w-full flex flex-row justify-between">
           <motion.div className="md:w-1/3 flex flex-row">
             <motion.img variants={navItemVariants} src="/assets/logo.jpeg" className="h-full md:ml-3 " alt="logo" />
-            <motion.p variants={navItemVariants} className="text-xl poppins-bold my-auto  text-center">Srinivas Institute <br /> of Technology</motion.p>
+            <Link href={'/'}>  <motion.p variants={navItemVariants} className="text-xl poppins-bold my-auto  text-center">Srinivas Institute <br /> of Technology</motion.p></Link>
           </motion.div>
           <div className="w-2/3 hidden md:block my-auto">
             <ul className="flex flex-row justify-around h-full ">
-              {links.map(link => <NavItem key={link}>{link}</NavItem>)}
+              {links.map(link => <NavItem key={link.link} href={link.link} link={link.name} />)}
               <li >
                 <motion.button variants={navItemVariants} whileHover='hover' className="btn p-3 rounded-md border-black border px-5 mr-3">Sign In</motion.button>
                 <motion.button variants={navItemVariants} whileHover='hover' className="btn p-3 rounded-md bg-black text-white">Join Now</motion.button>
@@ -86,9 +100,9 @@ const NavItem = ({ children }: { children: React.ReactNode }) => (
             <span className="text-end font-extrabold bg-gray-300 rounded-full m-3  p-3 text-white" onClick={() => setShowMenu(!showMenu)}><svg height="15px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="12px" xmlns="http://www.w3.org/2000/svg"><path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z" /></svg></span>
           </div>
           <ul className="flex flex-col mt-12 gap-5 divide-y" >
-            {links.map(link => <NavItem key={link}>{link}</NavItem>)}
-            <NavItem>Sign In</NavItem>
-            <NavItem>Join Now</NavItem>
+            {links.map(link => <NavItem key={link.link} href={link.link} link={link.name} />)}
+            <NavItem href="/login" link="Login" />
+            <NavItem href="/signup" link="Sign UP" />
           </ul>
         </div>
       </motion.nav>
